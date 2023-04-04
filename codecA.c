3312353fbd6f0a74b887543d440b5277ee01262e
ubuntu <itamar.kraitman@msmail.ariel.ac.il>
2023-04-04 16:22:04 +0200

    first commit

---
+++ codecA.c
@@ -0,0 +1,2 @@
+// codecA: covert all lower case chars to upper case, and all upper case to lower case.
+// All other chars will remain unchanged.
---
+++ codecB.c
@@ -0,0 +1,2 @@
+// codecB: convert all chars to the 3-rd next char (adding a number of 3 to the ascii
+// val).
