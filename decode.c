608ad9282b61be55268a9bedfa19aad785bf1dba
ubuntu <itamar.kraitman@msmail.ariel.ac.il>
2023-04-13 17:02:31 +0200

    changing to real time

--- decode.c
+++ decode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <dlfcn.h>
 #include "codecA.h"
 #include "codecB.h"
 
@@ -11,17 +12,49 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    // load the libraries
+    void* decodecA_handle = dlopen("libcodecA.so", RTLD_NOW);
+    if (decodecA_handle == NULL)
+    {
+        printf("Error has occured while opening decodecA_handler");
+        return 1;
+    }
+    
+    void* decodecB_handle = dlopen("libcodecB.so", RTLD_NOW);
+    if (decodecB_handle == NULL)
+    {
+        printf("Error has occured while opening decodecB_handler");
+        return 1;
+    }
+
+    // get function pinters
+    typedef char* (*decodeA)(char*);
+    decodeA dcodeA = (decodeA)dlsym(decodecA_handle, "codeA");
+    if (dcodeA == NULL)
+    {
+        printf("Error has occured while creating pinter to decodeA");
+        return 1;
+    }
+
+    typedef char* (*decodeB)(char*);
+    decodeB dcodeB = (decodeB)dlsym(decodecB_handle, "codeB");
+    if (dcodeB == NULL)
+    {
+        printf("Error has occured while creating pinter to decodeB");
+        return 1;
+    }
+
     char *decodec = argv[1];
     char *message = argv[2];
     char *originalText;
 
     if (strcmp(decodec, "decodecA") == 0)
     {
-        originalText = decodeA(message);
+        originalText = dcodeA(message);
     }
     else if (strcmp(decodec, "decodecB") == 0)
     {
-        originalText = encodeB(message);
+        originalText = dcodeB(message);
     }
     else
     {
@@ -29,4 +62,10 @@ int main(int argc, char const *argv[])
         return 1;
     }
     printf("%c", originalText);
+
+
+    dlclose(decodecA_handle);
+    dlclose(decodecB_handle);
+
+    return 0;
 }
--- encode.c
+++ encode.c
@@ -12,17 +12,50 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    // load the libraries
+    void* codecA_handle = dlopen("libcodecA.so", RTLD_NOW);
+    if (codecA_handle == NULL)
+    {
+        printf("Error has occured while opening codecA_handler");
+        return 1;
+    }
+    
+    void* codecB_handle = dlopen("libcodecB.so", RTLD_NOW);
+    if (codecB_handle == NULL)
+    {
+        printf("Error has occured while opening codecB_handler");
+        return 1;
+    }
+
+    // get function pinters
+    typedef char* (*encodeA)(char*);
+    encodeA codeA = (encodeA)dlsym(codecA_handle, "codeA");
+    if (codeA == NULL)
+    {
+        printf("Error has occured while creating pinter to encodeA");
+        return 1;
+    }
+
+    typedef char* (*encodeB)(char*);
+    encodeB codeB = (encodeB)dlsym(codecB_handle, "codeA");
+    if (codeA == NULL)
+    {
+        printf("Error has occured while creating pinter to encodeA");
+        return 1;
+    }
+
+
     char *codec = argv[1];
     char *message = argv[2];
     char *code;
 
     if (strcmp(codec, "codecA") == 0)
     {
-        code = encodeA(message);
+        code = codeA(message);
     }
     else if (strcmp(codec, "codecB") == 0)
     {
-        code = encodeB(message);
+        code = codeB(message);
     }
     else
     {
@@ -30,4 +63,8 @@ int main(int argc, char const *argv[])
         return 1;
     }
     printf("%c", code);
+
+    dlclose(codecA_handle);
+    dlclose(codecB_handle);
+    return 0;
 }
