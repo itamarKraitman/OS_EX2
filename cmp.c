ed176573651221870b328c8a399d4b4d0f030695
Yulia <yukatz578@gmail.com>
2023-04-17 14:43:42 -0700

    added part 1

---
+++ cmp.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 1024
+
+bool isEqual(FILE* f1, FILE* f2, int ignoreCaseFl);
+void pribtVerbose(bool result);
+
+
+// argc - argument count, helps to indicate validate of the input.
+// argv - vector of arguments input
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3 && argc > 4)
+    {
+        printf("usage: cmp <file1> <file2>, or");
+        printf("usage: cmp <file1> <file2> -i, or");
+        printf("usage: cmp <file1> <file2> -v, or");
+        printf("usage: cmp <file1> <file2> -i -v, or");
+        printf("usage: cmp <file1> <file2> -v -i");
+        return 1;
+    }
+
+    char *file1 = argv[1]; // First file name
+    char *file2 = argv[2]; // Second file nam
+    int verbosFl = 0;      // To print equal/distinct or 1\0
+    int ignoreCaseFl = 0;  // Ignore upper case or not
+    FILE *f1 = fopen(file1, "r");
+    FILE *f2 = fopen(file2, "r");
+
+    // If the file are good
+    if (f1 == NULL || f2 == NULL)
+    {
+        printf("One or both files are unreachable");
+        return 1;
+    }
+
+    // If the flag are valid and the flags
+    if (argc == 3)
+    {
+        if (strcmp(argv[3], "-v") == 0)
+        {
+            verbosFl = 1;
+        }
+        else if (strcmp(argv[3], "-i") == 0)
+        {
+            ignoreCaseFl = 1;
+        }
+        else
+        {
+            printf("usage: cmp <file1> <file2>, or");
+            printf("usage: cmp <file1> <file2> -i, or");
+            printf("usage: cmp <file1> <file2> -v, or");
+            printf("usage: cmp <file1> <file2> -i -v, or");
+            printf("usage: cmp <file1> <file2> -v -i");
+            return 1;
+        }
+    }
+    if (argc == 4)
+    {
+        if ((strcmp(argv[3], "-v") == 0 && strcmp(argv[4], "-i") == 0) ||
+            (strcmp(argv[3], "-i") == 0 && strcmp(argv[4], "-v") == 0))
+        {
+            verbosFl = 1;
+            ignoreCaseFl = 1;
+        }
+        else
+        {
+            printf("usage: cmp <file1> <file2>, or");
+            printf("usage: cmp <file1> <file2> -i, or");
+            printf("usage: cmp <file1> <file2> -v, or");
+            printf("usage: cmp <file1> <file2> -i -v, or");
+            printf("usage: cmp <file1> <file2> -v -i");
+            return 1;
+        }
+    }
+
+    bool equal = isEqual(f1, f2, ignoreCaseFl);
+    if (verbosFl)
+    {
+        printVerbose(equal);
+    }
+
+    fclose(f1);
+    fclose(f2);
+
+    return equal ? 0 : 1;
+}
+
+bool isEqual(FILE *f1, FILE *f2, int ignoreCase)
+{
+    char buf1[BUF_SIZE];
+    char buf2[BUF_SIZE];
+    int len1, len2;
+
+    while (true)
+    {
+        len1 = fread(buf1, 1, BUF_SIZE, f1);
+        len2 = fread(buf2, 1, BUF_SIZE, f2);
+
+        if (len1 != len2)
+        {
+            return 0;
+        }
+
+        if (feof(f1) || feof(f2))
+        {
+            break;
+        }
+
+        if (ignoreCase == 1)
+        {
+            for (int i = 0; i < len1; i++)
+            {
+                buf1[i] = tolower(buf1[i]);
+                buf2[i] = tolower(buf2[i]);
+            }
+        }
+
+        if (memcmp(buf1, buf2, len1) != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printVerbose(bool filesEqual)
+{
+    if (isEqual)
+    {
+        printf("equal\n");
+    }
+    else
+    {
+        printf("distinct\n");
+    }
+}
+
+
---
+++ copy.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 1024
+
+bool copyFile(FILE* f1, FILE* f2, int overwriteFl);
+void pribtVerbose(bool result);
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3 && argc > 4)
+    {
+        printf("usage: cmp <file1> <file2>, or");
+        printf("usage: cmp <file1> <file2> -f, or");
+        printf("usage: cmp <file1> <file2> -v, or");
+        printf("usage: cmp <file1> <file2> -i -v, or");
+        printf("usage: cmp <file1> <file2> -v -i");
+        return 1;
+    }
+
+    char *file1 = argv[1]; // First file name
+    char *file2 = argv[2]; // Second file nam
+    int verbosFl = 0;      // To print success/target file exist/general failure or 1\0
+    int overwriteFl = 0;  // Force copy that allows to overwrite the target file
+    bool fileEx = 0;
+    FILE *sourceFile = fopen(file1, "r");
+    FILE *destFile = fopen(file2, "r");
+
+    
+
+
+    // If the flag are valid and the flags
+    if (argc == 3)
+    {
+        if (strcmp(argv[3], "-v") == 0)
+        {
+            verbosFl = 1;
+        }
+        else if (strcmp(argv[3], "-i") == 0)
+        {
+            overwriteFl = 1;
+        }
+        else
+        {
+            printf("usage: cmp <file1> <file2>, or");
+            printf("usage: cmp <file1> <file2> -i, or");
+            printf("usage: cmp <file1> <file2> -v, or");
+            printf("usage: cmp <file1> <file2> -i -v, or");
+            printf("usage: cmp <file1> <file2> -v -i");
+            return 1;
+        }
+    }
+    if (argc == 4)
+    {
+        if ((strcmp(argv[3], "-v") == 0 && strcmp(argv[4], "-i") == 0) ||
+            (strcmp(argv[3], "-i") == 0 && strcmp(argv[4], "-v") == 0))
+        {
+            verbosFl = 1;
+            overwriteFl = 1;
+        }
+        else
+        {
+            printf("usage: cmp <file1> <file2>, or");
+            printf("usage: cmp <file1> <file2> -i, or");
+            printf("usage: cmp <file1> <file2> -v, or");
+            printf("usage: cmp <file1> <file2> -i -v, or");
+            printf("usage: cmp <file1> <file2> -v -i");
+            return 1;
+        }
+    }
+
+
+    // If the source exists
+    if (sourceFile == NULL)
+    {
+        if(verbosFl == 1) printf("general failure\n");
+        return 1;
+    }
+
+
+    if (destFile != NULL)
+    {
+        if(overwriteFl == 1){
+            remove(file2);
+        }else {
+            if(verbosFl == 1) printf("target file exist\n");
+            return 1;
+        }
+    }
+
+
+    bool copy = copyFile(sourceFile, destFile, overwriteFl);
+
+    if (verbosFl)
+    {
+        printVerbose(copy, fileEx);
+    }
+
+    fclose(sourceFile);
+    fclose(destFile);
+
+    return copy ? 0 : 1;
+}
+
+bool copyFile(FILE *sourceFile, FILE *destFile, int overwriteFl)
+{
+    char buf1[BUF_SIZE];
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buf1, 1, sizeof(buf1), sourceFile)) > 0) {
+        fwrite(buf1, 1, bytes_read, destFile);
+    }
+ 
+}
+
+void printVerbose(bool copy, bool fileEx)
+{
+    if (copy)
+    {
+        printf("success”\n");
+    }
+    else if(fileEx)
+    {
+    printf("target file exist\n");
+    }
+    else printf("general failure\n");
+
+}
+
+
